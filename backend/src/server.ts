e40fc370e4ff563c2c5fcb9342fdbc5fc75aaf6a
VORTX\rm <rm@vortx.com.br>
2020-08-07 14:01:22 -0300

    Done until 1:52:55

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
 
+app.use(routes);
+
 app.listen(3333, () => {
     console.log('Server started!')
 })
-
-//1:26:07
