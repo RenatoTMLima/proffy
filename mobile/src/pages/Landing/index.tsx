6dc8d41ddbb8cdfee3927e2143dae5d4f567adc1
Renato <rm@vortx.com.br>
2020-08-12 20:34:57 -0300

    Finished 4th day

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
 import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing = () => {
+  const { navigate } = useNavigation();
+
+  const handleNavigateToGiveClassesPage = () => {
+    navigate('GiveClasses')
+  }
+
+  const handleNavigateToStudyPage = () => {
+    navigate('Study')
+  }
+  
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -14,6 +28,28 @@ const Landing = () => {
       <Text style={styles.title}>
         Seja bem vindo(a), {'\n'}
         <Text style={styles.titleBold} >O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton 
+          style={[styles.button, styles.buttonPrimary]}
+          onPress={handleNavigateToStudyPage}
+        >
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+        <RectButton 
+          style={[styles.button, styles.buttonSecondary]} 
+          onPress={handleNavigateToGiveClassesPage}
+        >
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 280 conexões já realizadas {' '}
+        <Image source={heartIcon} />
       </Text>
     </View>
   )
